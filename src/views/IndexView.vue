99df47d7c2542da7f3a86c2bde3e97914210ba32
timre13 <itorteli13@gmail.com>
2022-11-15 09:42:30 +0100

    Start working on navbar

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,10 +1,12 @@
 <template>
     <main>
+        <NavBar></NavBar>
         <h1>Hello World</h1>
     </main>
 </template>
 
 <script setup lang="ts">
+  import NavBar from '@/components/NavBar.vue';
 </script>
 
 <style scoped lang="scss">
