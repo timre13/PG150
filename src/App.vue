b8c8f036248bfc1c82a0b21feefa25fdda45d059
0324bence <bence20050324@gmail.com>
2022-11-17 21:11:56 +0100

    Create basic layout (Not final)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+    import { RouterLink, RouterView } from "vue-router";
+    import Footer from "./components/Footer.vue";
+    import NavBar from "./components/NavBar.vue";
 </script>
 
 <template>
-  <RouterView />
+    <div id="screen">
+        <RouterView class="view" />
+        <NavBar class="nav" />
+    </div>
+    <Footer />
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+    #screen {
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        align-items: stretch;
 
+        .view {
+            flex: 1;
+        }
+    }
 </style>
