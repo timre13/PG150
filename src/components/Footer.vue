b574835c15ec0f5622fc10fd6a73d0001291170e
0324bence <bence20050324@gmail.com>
2022-11-17 21:12:16 +0100

    Add footer box with color

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,13 @@
 <template>
-    <div></div>
+    <div id="footer"></div>
 </template>
+
 <script setup lang="ts"></script>
-<style lang="scss" scoped></style>
+
+<style lang="scss" scoped>
+    #footer {
+        width: 100%;
+        height: 15rem;
+        background-color: hsl(0, 0%, 30%);
+    }
+</style>
