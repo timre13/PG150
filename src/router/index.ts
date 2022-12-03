699d190aeb446148b6b32f6de48f5824f8e6947e
0324bence <bence20050324@gmail.com>
2022-12-03 18:54:15 +0100

    Change history mode to "hash"

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory, createWebHistory } from "vue-router";
 import IndexView from "../views/IndexView.vue";
 import EmlekekView from "../views/EmlekekView.vue";
 import OsztalyokView from "../views/OsztalyokView.vue";
@@ -6,7 +6,7 @@ import TablokView from "../views/TablokView.vue";
 import VideokView from "../views/VideokView.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: "/",
